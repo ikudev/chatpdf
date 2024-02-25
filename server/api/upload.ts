30d6ef262d1557767321cc978d4c5c9ca3b825bc
Ikutetsu <allangreenland@gmail.com>
2024-02-24 19:39:23 -0500

    split document

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import { readFiles } from 'h3-formidable';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
 export default defineEventHandler(async (event) => {
   const { files } = await readFiles(event, {
@@ -11,8 +13,19 @@ export default defineEventHandler(async (event) => {
     .flatten()
     .compact()
     .value()
-    .forEach((file) => {
-      const { originalFilename, newFilename } = file;
-      console.log({ originalFilename, newFilename });
+    .forEach(async (file) => {
+      const loader = new PDFLoader(file.filepath);
+      const docs = await loader.load();
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 50,
+      });
+      const chunks = await splitter.splitDocuments(docs);
+      chunks.map((chunk, i) => {
+        console.log(
+          `page ${i}:`,
+          _.truncate(chunk.pageContent, { length: 20 })
+        );
+      });
     });
 });
