39ffcc154b21da51990d43367bd64d3d47cea455
Ikutetsu <allangreenland@gmail.com>
2024-02-24 21:49:22 -0500

    implemented embeddings and vector store

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -2,6 +2,9 @@ import _ from 'lodash';
 import { readFiles } from 'h3-formidable';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PrismaVectorStore } from '@langchain/community/vectorstores/prisma';
+import { Prisma, PrismaClient, type Document } from '@prisma/client';
 
 export default defineEventHandler(async (event) => {
   const { files } = await readFiles(event, {
@@ -14,6 +17,7 @@ export default defineEventHandler(async (event) => {
     .compact()
     .value()
     .forEach(async (file) => {
+      // split the uploaded file into chunks
       const loader = new PDFLoader(file.filepath);
       const docs = await loader.load();
       const splitter = new RecursiveCharacterTextSplitter({
@@ -21,11 +25,30 @@ export default defineEventHandler(async (event) => {
         chunkOverlap: 50,
       });
       const chunks = await splitter.splitDocuments(docs);
-      chunks.map((chunk, i) => {
-        console.log(
-          `page ${i}:`,
-          _.truncate(chunk.pageContent, { length: 20 })
-        );
-      });
+      // generate embeddings and the vector store
+      const openAIApiKey = useRuntimeConfig().openaiApiKey;
+      const embeddings = new OpenAIEmbeddings({ openAIApiKey });
+      const db = new PrismaClient();
+      const vectorStore = PrismaVectorStore.withModel<Document>(db).create(
+        embeddings,
+        {
+          prisma: Prisma,
+          tableName: 'Document',
+          vectorColumnName: 'vector',
+          columns: {
+            id: PrismaVectorStore.IdColumn,
+            content: PrismaVectorStore.ContentColumn,
+          },
+        }
+      );
+      // store the chunks in the database
+      await db.document.deleteMany();
+      await vectorStore.addModels(
+        await db.$transaction(
+          chunks.map((chunk) =>
+            db.document.create({ data: { content: chunk.pageContent } })
+          )
+        )
+      );
     });
 });
