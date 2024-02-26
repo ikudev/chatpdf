c72bea86e9628d721c87e19fc79a2ba4e82318a3
Ikutetsu <allangreenland@gmail.com>
2024-02-26 18:09:55 -0500

    complete the retrieval

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,7 +1,10 @@
-import { LangChainStream, Message, StreamingTextResponse } from 'ai';
-import { ChatOpenAI } from '@langchain/openai';
+import { Message, StreamingTextResponse } from 'ai';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { PromptTemplate } from '@langchain/core/prompts';
 import _ from 'lodash';
+import { Prisma, PrismaClient, type Document } from '@prisma/client';
+import { PrismaVectorStore } from '@langchain/community/vectorstores/prisma';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 
 export default defineLazyEventHandler(() => {
   const apiKey = useRuntimeConfig().openaiApiKey;
@@ -13,10 +16,23 @@ export default defineLazyEventHandler(() => {
     streaming: true,
   });
 
+  const db = new PrismaClient();
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey: apiKey });
+  const vectorStore = PrismaVectorStore.withModel<Document>(db).create(
+    embeddings,
+    {
+      prisma: Prisma,
+      tableName: 'Document',
+      vectorColumnName: 'vector',
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    }
+  );
+
   return defineEventHandler(async (event) => {
     const { messages } = await readBody(event);
-
-    const { stream, handlers } = LangChainStream();
 
     const standaloneQuestionTemplate = `Given a question, convert the question to a standalone question.
       question: {question}
@@ -24,14 +40,17 @@ export default defineLazyEventHandler(() => {
     const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
       standaloneQuestionTemplate
     );
+    const retriever = vectorStore.asRetriever();
+    const outputParser = new StringOutputParser();
 
-    standaloneQuestionPrompt
+    const chain = standaloneQuestionPrompt
       .pipe(llm)
-      .invoke(
-        { question: _.last<Message>(messages)?.content || '' },
-        { callbacks: [handlers] }
-      )
-      .catch(console.error);
+      .pipe(outputParser)
+      .pipe(retriever)
+      .pipe((docs) => docs.map((e) => e.pageContent).join('\n\n'));
+    const stream = await chain.stream({
+      question: _.last<Message>(messages)?.content || '',
+    });
     return new StreamingTextResponse(stream);
   });
 });
