1e554b47b245d86a13871a70718593b443668e40
Ikutetsu <allangreenland@gmail.com>
2024-02-28 15:52:15 -0500

    use RunnableSequence class

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,10 +1,15 @@
+import _ from 'lodash';
 import { Message, StreamingTextResponse } from 'ai';
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { PromptTemplate } from '@langchain/core/prompts';
-import _ from 'lodash';
 import { Prisma, PrismaClient, type Document } from '@prisma/client';
 import { PrismaVectorStore } from '@langchain/community/vectorstores/prisma';
 import { StringOutputParser } from '@langchain/core/output_parsers';
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from '@langchain/core/runnables';
+import { Document as DocumentChunk } from '@langchain/core/documents';
 
 export default defineLazyEventHandler(() => {
   const apiKey = useRuntimeConfig().openaiApiKey;
@@ -33,14 +38,27 @@ export default defineLazyEventHandler(() => {
 
   return defineEventHandler(async (event) => {
     const { messages } = await readBody(event);
-    // standalone question prompt
+    const outputParser = new StringOutputParser();
+
+    // standalone question chain
     const standaloneQuestionTemplate = `Given a question, convert the question to a standalone question.
       question: {question}
       standalone question:`;
     const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
       standaloneQuestionTemplate
     );
-    // answer prompt
+    const questionChain = standaloneQuestionPrompt.pipe(llm).pipe(outputParser);
+
+    // retrieval chain
+    const retriever = vectorStore.asRetriever();
+    const retrievalChain = RunnableSequence.from([
+      (prevResult) => prevResult.standaloneQuestion,
+      retriever,
+      (docs: DocumentChunk[]) =>
+        docs.map((doc) => doc.pageContent).join('\n\n'),
+    ]);
+
+    // answering chain
     const answerTemplate = `You are a helpful support assistant who can answer a given question based on the context provided.
       Try to find the answer in the context.
       If you can't find the answer, say "I'm sorry, I don't know the answer to that."
@@ -49,26 +67,20 @@ export default defineLazyEventHandler(() => {
       question: {question}
       answer: `;
     const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
-    // retrieval
-    const retriever = vectorStore.asRetriever();
-    const outputParser = new StringOutputParser();
-    // chain
-    let standaloneQuestion = '';
-    const chain = standaloneQuestionPrompt
-      .pipe(llm)
-      .pipe(outputParser)
-      .pipe((qst) => {
-        standaloneQuestion = qst;
-        return qst;
-      })
-      .pipe(retriever)
-      .pipe((docs) => ({
-        question: standaloneQuestion,
-        context: docs.map((e) => e.pageContent).join('\n\n'),
-      }))
-      .pipe(answerPrompt)
-      .pipe(llm)
-      .pipe(outputParser);
+    const answerChain = answerPrompt.pipe(llm).pipe(outputParser);
+
+    // overall chain
+    const chain = RunnableSequence.from([
+      {
+        standaloneQuestion: questionChain,
+        originalInput: new RunnablePassthrough(),
+      },
+      {
+        context: retrievalChain,
+        question: ({ originalInput }) => originalInput.question,
+      },
+      answerChain,
+    ]);
     const stream = await chain.stream({
       question: _.last<Message>(messages)?.content || '',
     });
